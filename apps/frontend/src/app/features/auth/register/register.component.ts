a3e112c91eaea9072ba8058ae31f9a585889fa38
Caio Oliveira <caio2601@gmail.com>
2025-05-28 21:12:33 -0300

    feat: implementa RegisterComponent com o formulário reatico e AuthService

---
+++ apps/frontend/src/app/features/auth/register/register.component.ts
@@ -0,0 +1,50 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+    FormBuilder,
+    ReactiveFormsModule,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import { RouterModule, Router } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+
+@Component({
+    selector: 'app-register',
+    imports: [CommonModule, ReactiveFormsModule, RouterModule],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.scss',
+})
+export class RegisterComponent {
+    form: FormGroup;
+
+    constructor(
+        private fb: FormBuilder,
+        private router: Router,
+        private auth: AuthService
+    ) {
+        this.form = this.fb.group({
+            name: ['', [Validators.required, Validators.minLength(3)]],
+            email: ['', [Validators.required, Validators.email]],
+            password: ['', [Validators.required, Validators.minLength(6)]],
+        });
+    }
+
+    onSubmit() {
+        if (this.form.invalid) {
+            this.form.markAllAsTouched();
+            return;
+        }
+
+        const { name, email, password } = this.form.value;
+
+        this.auth.register({ name, email, password }).subscribe({
+            next: () => {
+                this.router.navigateByUrl('/login');
+            },
+            error: err => {
+                alert(err.error.message || 'Erro ao fazer cadastro');
+            },
+        });
+    }
+}
