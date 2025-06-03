b3286b446ec6e7a521b8919e3336b4cfe61fae63
Caio Oliveira <caio2601@gmail.com>
2025-06-03 16:35:36 -0300

    feat: ajusta as rotas para usar AuthLayoutComponent e AppLayoutComponent

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,14 +1,35 @@
 import { Routes } from '@angular/router';
-import { authGuard } from './core/auth.guard';
-import { LoginComponent } from '../app/features/auth/login/login.component';
+import { AuthLayoutComponent } from './layouts/auth-layout.component';
+import { AppLayoutComponent } from './layouts/app-layout.component';
+import { LoginComponent } from './features/auth/login/login.component';
 import { RegisterComponent } from './features/auth/register/register.component';
+import { authGuard } from './core/auth.guard';
+// Em breve: import { DashboardComponent }      from './features/dashboard/dashboard.component';
 
 export const routes: Routes = [
-    { path: 'login', component: LoginComponent },
-    { path: 'register', component: RegisterComponent },
+    {
+        path: 'auth',
+        component: AuthLayoutComponent,
+        children: [
+            { path: 'login', component: LoginComponent },
+            { path: 'register', component: RegisterComponent },
+            { path: '', pathMatch: 'full', redirectTo: 'login' },
+        ],
+    },
     {
         path: '',
+        component: AppLayoutComponent,
         canMatch: [authGuard],
-        children: [{ path: '', pathMatch: 'full', redirectTo: 'dashboard' }],
+        children: [
+            {
+                path: 'dashboard',
+                pathMatch: 'full' /* component: DashboardComponent */,
+            },
+            // { path: 'categories', loadComponent: () => import('./features/categories/category-list.component').then(m => m.CategoryListComponent) },
+            // { path: 'transactions', loadComponent: () => import('./features/transactions/transaction-list.component').then(m => m.TransactionListComponent) },
+            { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
+        ],
     },
+    // Rotas “catch-all”
+    { path: '**', redirectTo: 'auth/login' },
 ];
