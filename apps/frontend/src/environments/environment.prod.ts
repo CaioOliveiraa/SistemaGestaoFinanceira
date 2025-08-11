c30ad5bf6554582bd6bdcdd06b2030e011ceee6e
Caio Oliveira <caio2601@gmail.com>
2025-08-11 15:15:08 -0300

    chore: Adiciona o endereço da API no Angular

--- apps/frontend/src/environments/environment.prod.ts
+++ apps/frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://seu-dominio.com/api',
+    apiUrl: 'finance-system-api-c4abe3hrc5dbf8be.brazilsouth-01.azurewebsites.net/api',
 };
